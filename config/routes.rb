2bbc2dd0c5bdcdfeba8a93c22d2959e4b67366bf
Modern Coding <michel.boretti@gmx.de>
2017-01-21 11:04:03 +0700

    BCrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'sign_up' => 'users#new', :as => 'sign_up'
+  root :to => 'users#new'
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
